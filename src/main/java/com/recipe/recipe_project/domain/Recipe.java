b657e997f7d00e34cbc0c5f1c9092363cae18a77
peterson1999 <51360811+peterson1999@users.noreply.github.com>
2023-02-11 23:49:53 +0800

    add POJOs

---
+++ src/main/java/com/recipe/recipe_project/domain/Recipe.java
@@ -0,0 +1,113 @@
+package com.recipe.recipe_project.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private Integer source;
+    private Integer url;
+    private Integer directions;
+    @Lob
+    private Byte [] image;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+    @OneToOne(cascade= CascadeType.ALL)
+    private Notes notes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(Integer prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public Integer getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(Integer cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public Integer getServings() {
+        return servings;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public Integer getSource() {
+        return source;
+    }
+
+    public void setSource(Integer source) {
+        this.source = source;
+    }
+
+    public Integer getUrl() {
+        return url;
+    }
+
+    public void setUrl(Integer url) {
+        this.url = url;
+    }
+
+    public Integer getDirections() {
+        return directions;
+    }
+
+    public void setDirections(Integer directions) {
+        this.directions = directions;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public Notes getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Notes notes) {
+        this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredient() {
+        return ingredients;
+    }
+
+    public void setIngredient(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+}
